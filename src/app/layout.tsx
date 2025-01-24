5e4b6fd4e3dca1377382e0c4e75e42cd8f28782b
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-01-24 21:13:39 +0600

    add jost font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${jost.variable} antialiased`}>
         <div className="flex flex-col min-h-screen">
           <Header />
           {children}
