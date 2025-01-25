beb548b4657a097579ed5ab07a18866661d1395f
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2025-01-25 17:33:30 +0500

    feat: add adaptive to Sales and Slider in homepage

--- src/shared/ui/Slider/ui/Slider.tsx
+++ src/shared/ui/Slider/ui/Slider.tsx
@@ -6,13 +6,47 @@ import { ISliders } from "../types/type";
 const Slider = ({ slides, className }: ISliders) => {
   return (
     <div className={className}>
-      <Swiper spaceBetween={50} slidesPerView={6} loop={true}>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={6}
+        loop={true}
+        breakpoints={{
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          // when window width is >= 768px
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+          },
+          // when window width is >= 1024px
+          1366: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+          },
+          // when window width is >= 1440px
+          1440: {
+            slidesPerView: 6,
+            spaceBetween: 50,
+          },
+        }}
+      >
         {slides.map(slide => (
           <SwiperSlide key={slide.price}>
             <Swiper spaceBetween={50} slidesPerView={1}>
               {slide.img.map(img => (
                 <SwiperSlide key={img.src}>
-                  <img src={img.src} alt={img.alt} className="max-w-[300px] w-full h-[440px] object-cover" />
+                  <img
+                    src={img.src}
+                    alt={img.alt}
+                    className="max-w-[300px] w-full h-[440px] object-cover max-desktop:h-[280px]"
+                  />
                 </SwiperSlide>
               ))}
             </Swiper>
