fe0efd54251f6cff9967d2207e489375eeb43976
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2025-01-25 23:02:16 +0500

    change btn in product

--- src/features/ButtonsProduct/ui/ButtonsProduct.tsx
+++ src/features/ButtonsProduct/ui/ButtonsProduct.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import { useProductStore } from "@/entities/productStore/store";
+import React, { useState } from "react";
 
 const ButtonsProduct = () => {
+  const { addProduct } = useProductStore();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleAddProduct = () => {
+    if (name.trim() && price) {
+      addProduct({
+        id: Date.now().toString(),
+        name,
+        price: parseFloat(price),
+      });
+      setName("");
+      setPrice("");
+    }
+  };
   return (
     <div className="flex flex-col items-center gap-[15px]">
       <div className="flex gap-4">
         <div className="">
-          <button className="btn bg-transparent rounded-none btn-active uppercase">Добавить в корзину</button>
+          <button className="btn bg-transparent rounded-none btn-active uppercase" onClick={handleAddProduct}>
+            Добавить в корзину
+          </button>
         </div>
         <div className="">
           <button className="btn bg-transparent border-none shadow-none hover:bg-transparent btn-active uppercase">
