78a90ee8ad706b817fb554eeafb1c8af10cfebce
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2025-01-30 21:07:37 +0500

    feat: create ContactPage

--- src/widgets/ContactsPage/ui/ContactsPage.tsx
+++ src/widgets/ContactsPage/ui/ContactsPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ContactsPage = () => {
-  return <div>ContactsPage</div>;
+  return <div className="flex flex-col items-center">Hello</div>;
 };
 
 export default ContactsPage;
