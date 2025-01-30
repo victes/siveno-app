8fdc7ea485816d36e2fb05720140b5832506d614
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-01-30 20:39:35 +0600

    add shipping page and refactor footer

---
+++ src/app/(page)/shipping/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "@/shared/ui/Container";
+import ShippingPage from "@/widgets/ShippingPage";
+
+const page = () => {
+  return (
+    <Container>
+      <ShippingPage />
+    </Container>
+  );
+};
+
+export default page;
