65899a56b02f527405d461c5efde6d5b245b4f9d
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-02-04 21:23:47 +0600

    optimize about

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: "build",
+  images: {
+    domains: ["lesyanebo.com"], // Добавь домен изображения
+  },
 };
 
 export default nextConfig;
