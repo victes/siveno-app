100ffabd3c8a634c1cfc8ecd51d404dbae9e7e3b
bubaololo <bubaololo@gmail.com>
2025-03-20 23:12:38 +0300

    fix

--- src/app/(page)/catalog-products/page.tsx
+++ src/app/(page)/catalog-products/page.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AllProductsPage from "@/widgets/AllProductsPage";
 import { Container } from "@/shared/ui/Container";
+
+// Компонент-заглушка для загрузки
+const Loading = () => <div className="p-4 text-center">Загрузка...</div>;
 
 const page = () => {
   return (
     <Container>
-      <AllProductsPage />
+      <Suspense fallback={<Loading />}>
+        <AllProductsPage />
+      </Suspense>
     </Container>
   );
 };
