a0472ee0c67b731d204badafba4369d86f38e35b
JupiterIT5 <geno74@bk.ru>
2025-03-28 13:21:08 +0500

    update payment

--- src/app/(page)/success-payment/page.tsx
+++ src/app/(page)/success-payment/page.tsx
@@ -2,19 +2,18 @@ import { useSuccessPaymentMutation } from "@/shared/api/OrdersApi/OrdersApi";
 import { useEffect } from 'react'
 
 const page = () => {
-  const [successPayment] = useSuccessPaymentMutation();
-  useEffect(() => {
-		if (localStorage.getItem("orderId")) {
-			successPayment({
-			 object: {
-				 id: localStorage.getItem("orderId"),
-				 status: "succeeded",
-			 },
-		 });
-		 window.location.href="/"
-	 }
-	},[])
-
+  // const [successPayment] = useSuccessPaymentMutation();
+  // useEffect(() => {
+	// 	if (localStorage?.getItem("orderId")) {
+	// 		successPayment({
+	// 		 object: {
+	// 			 id: localStorage.getItem("orderId"),
+	// 			 status: "succeeded",
+	// 		 },
+	// 	 });
+	// 	 window.location.href="/"
+	//  }
+	// },[])
 	return (
 		<>
 		 	<h2>Redirect in Main Page</h2>
