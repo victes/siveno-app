6c37dd6efcc3a72f6c1e5f1fc7f0851e8a764698
NbekWeb <nbekdev@gmail.com>
2025-07-15 10:21:15 +0500

    add api

--- src/widgets/Catalog/ui/Catalog.tsx
+++ src/widgets/Catalog/ui/Catalog.tsx
@@ -1,42 +1,30 @@
-import img1 from "../../../../public/images/MainPage/catalog1.jpg";
-import img3 from "../../../../public/images/MainPage/catalog2.jpg";
-import img2 from "../../../../public/images/MainPage/catalog3.jpg";
-import img4 from "../../../../public/images/MainPage/catalog4.jpg";
 import Image from "next/image";
 import Link from "next/link";
+import { useGetCategoriesQuery } from "@/shared/api/CategoriesApi/CategoriesApi";
 import "../style/catalog.scss";
 
 function Catalog() {
+  const { data: categories, isLoading, error } = useGetCategoriesQuery();
+
+  if (isLoading) return <div className="mt-10 containers px-[40px]">Загрузка...</div>;
+  if (error) return <div className="mt-10 containers px-[40px]">Ошибка загрузки категорий</div>;
+  if (!categories || categories.length === 0)
+    return <div className="mt-10 containers px-[40px]">Категории не найдены</div>;
+
   return (
     <div className="mt-10 containers px-[40px]">
       <div className="w-full flex mt-[40px] mb-[40px] justify-center">
         <h2 className="text-3xl title-h1 text-start uppercase tracking-wide lineyka">Каталог</h2>
       </div>
       <div className="catalog__container">
-        <Link href={"/catalog-products/vyazannye-izdeliya"}>
-          <div>
-            <h2 className="mb-5 text-[20px]">Вязаные изделия</h2>
-            <Image src={img1} alt="..." className="img"></Image>
-          </div>
-        </Link>
-        <Link href={"/catalog-products/bazovye-produkty"}>
-          <div>
-            <h2 className="mb-5 text-[20px]">Базовые изделия</h2>
-            <Image src={img2} alt="..." className="img"></Image>
-          </div>
-        </Link>
-        <Link href={"/catalog-products/delovye-i-vechernie"}>
-          <div>
-            <h2 className="mb-5 text-[20px]">Деловые и вечерние</h2>
-            <Image src={img3} alt="..." className="img"></Image>
-          </div>
-        </Link>
-        <Link href={"/catalog-products/Lingerie"}>
-          <div>
-            <h2 className="mb-5 text-[20px]">Нижнее бельё</h2>
-            <Image src={img4} alt="..." className="img"></Image>
-          </div>
-        </Link>
+        {categories.map(category => (
+          <Link key={category.id} href={`/catalog-products/${category.slug}`}>
+            <div>
+              <h2 className="mb-5 text-[20px]">{category.title}</h2>
+              <Image src={category.image} alt={category.title} className="img" width={400} height={400} />
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
